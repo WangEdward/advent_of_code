25d4af595968915ba8029030cdc5d8928dd69ba9
Edward <73746306+WangEdward@users.noreply.github.com>
2023-12-02 01:08:57 +0800

    add: 2023 day1

---
+++ advent_of_code/2023/1/one.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import re
+
+REGEX = r'[1-9]'
+
+sum = 0
+
+with open(Path(__file__).parent / "input.txt") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        nums = re.findall(REGEX, line)
+        num = f'{nums[0]}{nums[-1]}'
+        sum += int(num)
+
+print(sum)
---
+++ advent_of_code/2023/1/two.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+NUM_MAP = [None, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', None, '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+sum = 0
+
+with open(Path(__file__).parent / "input.txt") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        left = right = 0
+        left_index = 1e3
+        right_index = -1
+        for index_i,i in enumerate(NUM_MAP):
+            if i:
+                l_index = line.find(i)
+                r_index = line.rfind(i)
+                if l_index != -1 and l_index < left_index:
+                    left_index = l_index
+                    left = index_i % 10
+                if r_index > right_index:
+                    right_index = r_index
+                    right = index_i % 10
+        num = left * 10 + right
+        sum += num
+
+print(sum)
